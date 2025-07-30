64dca7c41a15c539e2db5691563063a846cb4a86
chimex53 <chimex53@gmail.com>
2025-07-30 23:48:42 +0100

    - Implement update product with image handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,6 @@ import Product from "../models/productModel.js";
 import { formatFileSize } from "../utils/fileUpload.js";
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from 'dotenv';
-import product from "../models/productModel.js";
 
 // Load environment variables
 dotenv.config();  
@@ -13,6 +12,7 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 
 const createProduct = asyncHandler(async (req, res) => {
     const { name, price, description, category, sku, quantity } = req.body;
@@ -66,6 +66,7 @@ const createProduct = asyncHandler(async (req, res) => {
     // Send response 
     res.status(201).json(product);
 });
+
  // get all products
  const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({ user: req.user._id }).sort("-createdAT");
@@ -88,5 +89,84 @@ const getProduct= asyncHandler(async(req, res) => {
     res.status(200).json(product);
 });
 
-export { createProduct, getProducts,getProduct};
+// delete a product
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        res.status(404);
+        throw new Error("product not found");
+    }
+    
+    // match user with product
+    if (product.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("user not authorized");
+    }
+    await product.deleteOne();
+    res.status(200).json({ message: "Product removed" });
+});
+
+//update product 
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, description, category, quantity } = req.body;
+     
+    const product= await Product.findById(req.params.id)
+     if (!product) {
+        res.status(404);
+        throw new Error("product not found");
+    }
+
+    // match user with product
+    if (product.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("user not authorized");
+    }
+
+    // Handle image upload
+    let fileData = {};
+    if (req.file) {  
+        // save image to cloudinary
+        let uploadedFile;
+        try {
+            // Convert file to base64
+            const b64 = Buffer.from(req.file.buffer).toString("base64");
+            const dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+            uploadedFile = await cloudinary.uploader.upload(dataURI, {
+                folder: "inventory_system",
+                resource_type: "auto"
+            });
+        } catch (error) {
+            res.status(500);
+            throw new Error("Image could not be uploaded")
+        }
+        fileData = {
+            filename: req.file.originalname,
+            filePath: uploadedFile.secure_url,
+            fileType: req.file.mimetype,
+            fileSize: formatFileSize(req.file.size,2), // Format file size
+        }; 
+    }
+
+    //updated product
+
+   const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+        user: req.user._id,
+        name,
+        price,
+        description,
+        category,
+        quantity,
+        image: fileData || product.image,  // Include image data if present
+    },
+    {
+        new: true,         // return the updated document
+        runValidators: true, // run validators on the updated document
+    }
+);
+    // Send response 
+    res.status(200).json(updatedProduct);
+});
+export { createProduct, getProducts,getProduct, deleteProduct,updateProduct};
  
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { createProduct,getProducts, getProduct } from '../controllers/productController.js';
+import { createProduct } from '../controllers/productController.js';
 import { upload } from '../utils/fileUpload.js';
 import protect from '../middleWare/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', protect, upload.single('image'), createProduct);
-router.get('/', protect,  getProducts);
-router.get('/:id', protect,  getProduct);
 
 export default router;
- 
