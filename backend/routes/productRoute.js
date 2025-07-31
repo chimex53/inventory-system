75e343760ab1a31d3076312ea861a3c4baecff3b
chimex53 <chimex53@gmail.com>
2025-07-31 16:21:22 +0100

    updated product Routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createProduct } from '../controllers/productController.js';
+import { createProduct,getProducts,getProduct,deleteProduct,    updateProduct  } from '../controllers/productController.js';
 import { upload } from '../utils/fileUpload.js';
 import protect from '../middleWare/authMiddleware.js';
 
 const router = express.Router();
 
+// Product Routes
 router.post('/', protect, upload.single('image'), createProduct);
+router.get('/', protect, getProducts);
+router.get('/:id', protect, getProduct);
+router.delete('/:id', protect, deleteProduct);
+router.patch('/:id', protect, upload.single('image'), updateProduct);
 
 export default router;
